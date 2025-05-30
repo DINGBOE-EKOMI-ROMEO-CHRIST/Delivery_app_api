90623830265a4bfd6aef1f22138cf67d665e9206
DINGBOE-EKOMI-ROMEO-CHRIST <lekid397@gmail.com>
2025-05-30 16:00:22 +0100

    mise a jour du nommage des routes

--- livreurs/urls.py
+++ livreurs/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from livreurs.views import create_livreur_profile,update_livreur_profile, livreur_list
 
 urlpatterns = [
-    path('api/livreurs/create-profile/', create_livreur_profile, name='create_livreur_profile'),
-    path('api/livreurs/update-profile/', update_livreur_profile, name='update_livreur_profile'),
-    path('api/livreurs/', livreur_list, name='livreur_list'),
+    path('livreurs/create-profile/', create_livreur_profile, name='create_livreur_profile'),
+    path('livreurs/update-profile/', update_livreur_profile, name='update_livreur_profile'),
+    path('livreurs/', livreur_list, name='livreur_list'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('utilisateurs/', UtilisateurListCreate.as_view(), name='utilisateur-list-create'),
     path('utilisateurs/<int:pk>/', UtilisateurRetrieveUpdateDestroy.as_view(), name='utilisateur-retrieve-update-destroy'),
     path('me/', get_user_info, name='get_user_info'),
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
